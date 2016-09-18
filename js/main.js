c30e0c5316f4e6e978ec69271a3cbc6d378addbb
Greg Rowe <growe22@ufl.edu>
2016-09-17 22:07:12 -0500

    create president object var

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var iceCream = ['Cookie Two-Step', 'Cookie Dough', 'Cookies and Cream'];
+iceCream.push('Vanilla Bean');
+
+var president = {
+    firstName: 'Barack',
+    lastName: 'Obama',
+    termLength: 2,
+    party: 'Democrat',
+    yearsOfPresidency: 8
+};

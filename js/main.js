1c65c3d0e9599e236134f986e653b655d60ac422
Greg Rowe <growe22@ufl.edu>
2016-09-17 22:14:06 -0500

    added president variables to java

--- js/main.js
+++ js/main.js
@@ -1,10 +1,42 @@
 var iceCream = ['Cookie Two-Step', 'Cookie Dough', 'Cookies and Cream'];
 iceCream.push('Vanilla Bean');
 
-var president = {
+var president1 = {
     firstName: 'Barack',
     lastName: 'Obama',
     termLength: 2,
     party: 'Democrat',
+    yearsOfPresidency: 8
+};
+
+var president2 = {
+    firstName: 'George',
+    lastName: 'Bush',
+    termLength: 2,
+    party: 'Republican',
+    yearsOfPresidency: 8
+};
+
+var president3 = {
+    firstName: 'Bill',
+    lastName: 'Clinton',
+    termLength: 2,
+    party: 'Democrat',
+    yearsOfPresidency: 8
+};
+
+var president4 = {
+    firstName: 'George',
+    lastName: 'Bush',
+    termLength: 2,
+    party: 'Republican',
+    yearsOfPresidency: 8
+};
+
+var president5 = {
+    firstName: 'Ronald',
+    lastName: 'Regan',
+    termLength: 2,
+    party: 'Republican',
     yearsOfPresidency: 8
 };

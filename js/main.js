fa7b287ce21add5185f35e1e69dd97fccc97d1eb
Greg Rowe <growe22@ufl.edu>
2016-09-19 17:32:04 -0700

    created numba function

--- js/main.js
+++ js/main.js
@@ -50,3 +50,14 @@ function howdy(person, message) {
 
 var message = howdy('Greg Rowe ', ' hi!');
 console.log(message);
+
+var numba = function (string) {
+    if (string.length < 7) {
+        console.log('What a short little word!');
+}
+    if (string.length > 7) {
+            console.log("I'm sorry, but that's too many to count.");
+}
+    if (string.length == 7) {
+        console.log("7, what a perfect choice!");
+}
